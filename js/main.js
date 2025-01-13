3619fdbc04d484112f9391ccb3b730bab87de382
DarioS1K <dario79col@gmail.com>
2025-01-13 19:22:10 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay:200, origin: 'left'});
+sr.reveal('.hero-img', {delay:800, origin: 'right'});
